278c08a0bf9d2687b5dde760368dbd30c64e4ddf
Eshaan-B <eshaan.bhardy@gmail.com>
2022-05-14 14:46:53 +0530

    Added webview and FAB

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/homeScreen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://www.maptoglobe.com/BJ2vHR3Iq',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        onProgress: (int progress) {
+          print('WebView is loading (progress : $progress%)');
+        },
+        onPageStarted: (String url) {
+          print('Page started loading: $url');
+        },
+        onPageFinished: (String url) {
+          print('Page finished loading: $url');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          "Explore!",
+          style: TextStyle(fontSize: 9),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+    );
+  }
+}
